899efa9ae068f4063d807a6de397398fe9953190
Le Jeffe <jeffrey.gek@hotmail.com>
2023-12-14 18:26:26 +0100

    Adding font settings
    Adding font settings and increasing max timer

--- src/main/java/com/npcidletimer/NPCIdleTimerConfig.java
+++ src/main/java/com/npcidletimer/NPCIdleTimerConfig.java
@@ -5,11 +5,43 @@ import net.runelite.client.config.Alpha;
 import net.runelite.client.config.Config;
 import net.runelite.client.config.ConfigGroup;
 import net.runelite.client.config.ConfigItem;
+import net.runelite.client.config.ConfigSection;
 import net.runelite.client.config.Range;
 
 @ConfigGroup("npcidletimerplugin")
 public interface NPCIdleTimerConfig extends Config
 {
+	enum FontStyle {
+        BOLD("Bold"),
+        ITALICS("Italics"),
+        BOLD_ITALICS("Bold and italics"),
+        DEFAULT("Default");
+
+        String name;
+
+        FontStyle(String name) {
+            this.name = name;
+        }
+
+        public String getName() {
+            return name;
+        }
+    }
+
+	@ConfigSection(
+			name = "Timer settings",
+			description = "Settings relating to Timer",
+			position = 1
+	)
+	String timer_settings = "timer_settings";
+
+	@ConfigSection(
+            name = "font settings",
+            description = "Settings relating to fonts",
+            position = 2
+    )
+    String font_settings = "font_settings";
+
 	@ConfigItem(
 			position = 0,
 			keyName = "showOverlay",
@@ -25,7 +57,8 @@ public interface NPCIdleTimerConfig extends Config
 			position = 1,
 			keyName = "npcToShowTimer",
 			name = "NPC Names",
-			description = "Enter names of NPCs where you wish to use this plugin"
+			description = "Enter names of NPCs where you wish to use this plugin",
+			section = timer_settings
 	)
 	default String npcToShowTimer()
 	{
@@ -37,7 +70,8 @@ public interface NPCIdleTimerConfig extends Config
 			position = 2,
 			keyName = "normalTimerColor",
 			name = "Normal time color",
-			description = "Configures the color of the timer"
+			description = "Configures the color of the timer",
+			section = timer_settings
 	)
 	default Color normalTimerColor()
 	{
@@ -49,7 +83,8 @@ public interface NPCIdleTimerConfig extends Config
 			position = 3,
 			keyName = "lowTimerColor",
 			name = "Low time color",
-			description = "Configures the color of the timer when remaining time is low"
+			description = "Configures the color of the timer when remaining time is low",
+			section = timer_settings
 	)
 	default Color lowTimerColor()
 	{
@@ -57,13 +92,14 @@ public interface NPCIdleTimerConfig extends Config
 	}
 
 	@Range(
-			max = 300
+			max = 1000
 	)
 	@ConfigItem(
 			position = 4,
 			keyName = "maxDisplay",
 			name = "Time to start timer",
-			description = "This defines at what amount of seconds/ticks the timer will be shown. Before this time the npc won't show a timer"
+			description = "This defines at what amount of seconds/ticks the timer will be shown. Before this time the npc won't show a timer",
+			section = timer_settings
 	)
 	default int maxDisplay()
 	{
@@ -74,7 +110,8 @@ public interface NPCIdleTimerConfig extends Config
 			position = 5,
 			keyName = "customTimer",
 			name = "Enable custom timer",
-			description = "This option enables you to set a custom timer that fits the actual respawn timer of your npc or for other timing related manners"
+			description = "This option enables you to set a custom timer that fits the actual respawn timer of your npc or for other timing related manners",
+			section = timer_settings
 	)
 	default boolean customTimer()
 	{
@@ -85,7 +122,8 @@ public interface NPCIdleTimerConfig extends Config
 			position = 6,
 			keyName = "customTiming",
 			name = "Custom timer",
-			description = "The starting tick counter on the npc. The default timer of this plugin is set at 300 seconds. You may change this to fit the actual despawn timer"
+			description = "The starting tick counter on the npc. The default timer of this plugin is set at 300 seconds. You may change this to fit the actual despawn timer",
+			section = timer_settings
 	)
 	default int customTiming()
 	{
@@ -96,7 +134,8 @@ public interface NPCIdleTimerConfig extends Config
 			position = 7,
 			keyName = "lowDisplay",
 			name = "Timer low value",
-			description = "The maximum seconds at which the timer is considered low"
+			description = "The maximum seconds at which the timer is considered low",
+			section = timer_settings
 	)
 	default int lowDisplay()
 	{
@@ -110,7 +149,8 @@ public interface NPCIdleTimerConfig extends Config
 			position = 8,
 			keyName = "timerHeight",
 			name = "Height of timer",
-			description = "Change the vertical offset of the timer above the npc"
+			description = "Change the vertical offset of the timer above the npc",
+			section = timer_settings
 	)
 	default int timerHeight()
 	{
@@ -121,7 +161,8 @@ public interface NPCIdleTimerConfig extends Config
 			position = 9,
 			keyName = "showOverlayTicks",
 			name = "Show timer in ticks",
-			description = "Configures whether or not the timer should be shown in tick or seconds"
+			description = "Configures whether or not the timer should be shown in tick or seconds",
+			section = timer_settings
 	)
 	default boolean showOverlayTicks()
 	{
@@ -132,10 +173,54 @@ public interface NPCIdleTimerConfig extends Config
 			position = 10,
 			keyName = "showTimingType",
 			name = "Show type of timer used",
-			description = "Shows the type of timer used in the suffix of the actual time displayed"
+			description = "Shows the type of timer used in the suffix of the actual time displayed",
+			section = timer_settings
 	)
 	default boolean showTimingType()
 	{
 		return false;
+	}
+	@ConfigItem(
+			position = 11,
+			keyName = "customFont",
+			name = "Enable custom fonts",
+			description = "Enabling this setting makes it possible to use the custom font from the box below this",
+			section = font_settings
+	)
+	default boolean customFont() {
+		return true;
+	}
+
+	@ConfigItem(
+			position = 12,
+			keyName = "fontName",
+			name = "Font",
+			description = "Name of the font to use for the hp shown. Leave blank to use RuneLite setting.",
+			section = font_settings
+	)
+	default String fontName() {
+		return "roboto";
+	}
+
+	@ConfigItem(
+			position = 13,
+			keyName = "fontStyle",
+			name = "Font style",
+			description = "Style of the font to use for the hp shown. Only works with custom font.",
+			section = font_settings
+	)
+	default FontStyle fontStyle() {
+		return FontStyle.DEFAULT;
+	}
+
+	@ConfigItem(
+			position = 14,
+			keyName = "fontSize",
+			name = "Font size",
+			description = "Size of the font to use for XP drops. Only works with custom font.",
+			section = font_settings
+	)
+	default int fontSize() {
+		return 12;
 	}
 }
--- src/main/java/com/npcidletimer/NPCIdleTimerOverlay.java
+++ src/main/java/com/npcidletimer/NPCIdleTimerOverlay.java
@@ -5,8 +5,11 @@ import java.awt.Dimension;
 import java.awt.Graphics2D;
 import java.text.DecimalFormat;
 import java.text.NumberFormat;
+import java.awt.Font;
+import java.awt.RenderingHints;
 import javax.inject.Inject;
 import net.runelite.api.Point;
+import net.runelite.client.ui.FontManager;
 import net.runelite.client.ui.overlay.Overlay;
 import net.runelite.client.ui.overlay.OverlayLayer;
 import net.runelite.client.ui.overlay.OverlayPosition;
@@ -17,6 +20,13 @@ public class NPCIdleTimerOverlay extends Overlay
 
 	private final NPCIdleTimerPlugin plugin;
 	private final NPCIdleTimerConfig config;
+
+    protected String lastFont = "";
+    protected int lastFontSize = 0;
+    protected boolean useRunescapeFont = true;
+    protected NPCIdleTimerConfig.FontStyle lastFontStyle = NPCIdleTimerConfig.FontStyle.DEFAULT;
+    protected Font font = null;
+
 
 	NumberFormat format = new DecimalFormat("#");
 
@@ -29,9 +39,20 @@ public class NPCIdleTimerOverlay extends Overlay
 		this.config = config;
 	}
 
+	protected void handleFont(Graphics2D graphics) {
+        if (font != null) {
+            graphics.setFont(font);
+            if (useRunescapeFont) {
+                graphics.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_OFF);
+            }
+        }
+    }
+
 	@Override
 	public Dimension render(Graphics2D graphics)
 	{
+		updateFont();
+		handleFont(graphics);
 		if (config.showOverlay())
 		{
 			plugin.getWanderingNPCs().forEach((id, npc) -> renderTimer(npc, graphics));
@@ -84,6 +105,60 @@ public class NPCIdleTimerOverlay extends Overlay
 		if (canvasPoint != null && (maxDisplay >= timeLeft))
 		{
 			OverlayUtil.renderTextLocation(graphics, canvasPoint, timeLeftString, timerColor);
+		}
+	}
+	private void updateFont() {
+		//only perform anything within this function if any settings related to the font have changed
+		if (!lastFont.equals(config.fontName()) || lastFontSize != config.fontSize() || lastFontStyle != config.fontStyle()) {
+			if (config.customFont()) {
+				lastFont = config.fontName();
+			}
+			lastFontSize = config.fontSize();
+			lastFontStyle = config.fontStyle();
+
+			//use runescape font as default
+			if (config.fontName().equals("") || config.customFont() == false) {
+				if (config.fontSize() < 16) {
+					font = FontManager.getRunescapeSmallFont();
+				} else if (config.fontStyle() == NPCIdleTimerConfig.FontStyle.BOLD || config.fontStyle() == NPCIdleTimerConfig.FontStyle.BOLD_ITALICS) {
+					font = FontManager.getRunescapeBoldFont();
+				} else {
+					font = FontManager.getRunescapeFont();
+				}
+
+				if (config.fontSize() > 16) {
+					font = font.deriveFont((float) config.fontSize());
+				}
+
+				if (config.fontStyle() == NPCIdleTimerConfig.FontStyle.BOLD) {
+					font = font.deriveFont(Font.BOLD);
+				}
+				if (config.fontStyle() == NPCIdleTimerConfig.FontStyle.ITALICS) {
+					font = font.deriveFont(Font.ITALIC);
+				}
+				if (config.fontStyle() == NPCIdleTimerConfig.FontStyle.BOLD_ITALICS) {
+					font = font.deriveFont(Font.ITALIC | Font.BOLD);
+				}
+
+				useRunescapeFont = true;
+				return;
+			}
+
+			int style = Font.PLAIN;
+			switch (config.fontStyle()) {
+				case BOLD:
+					style = Font.BOLD;
+					break;
+				case ITALICS:
+					style = Font.ITALIC;
+					break;
+				case BOLD_ITALICS:
+					style = Font.BOLD | Font.ITALIC;
+					break;
+			}
+
+			font = new Font(config.fontName(), style, config.fontSize());
+			useRunescapeFont = false;
 		}
 	}
 
